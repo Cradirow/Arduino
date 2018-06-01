d7a361638e847abcd7b810c75bb8badf2c94e353
Cradirow <hanorang2@naver.com>
2018-06-01 21:58:15 +0900

    Add gas status

--- app/src/main/java/arduino/ahn/com/arduino/Fragment1.java
+++ app/src/main/java/arduino/ahn/com/arduino/Fragment1.java
@@ -2,9 +2,18 @@ package arduino.ahn.com.arduino;
 
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
 
 /**
  * Created by user on 2018-06-01.
@@ -17,56 +26,73 @@ public class Fragment1 extends Fragment {
     }
 
     @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+
+    View view;
+
+    @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_fragment1, container, false);
+        view = inflater.inflate(R.layout.fragment_fragment1, container, false);
+
+        textHumidity = (TextView)view.findViewById(R.id.textA);
+        textTemperature = (TextView)view.findViewById(R.id.textB);
+        imageView_Good = (ImageView)view.findViewById(R.id.imageViewTemp);
+        return view;
+    }
+
+    private FirebaseDatabase firebaseDatabase = FirebaseDatabase.getInstance();
+    private DatabaseReference databaseHumidity = firebaseDatabase.getReference().child("Humidity");
+    private DatabaseReference databaseTemperature = firebaseDatabase.getReference().child("Temperature");
+
+    TextView textHumidity;
+    TextView textTemperature;
+
+    ImageView imageView_Good;
+
+    float hum;
+    float temp;
+
+    @Override
+    public void onStart() {
+        super.onStart();
+
+        databaseHumidity.addValueEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange(DataSnapshot dataSnapshot) {
+
+                hum = dataSnapshot.getValue(float.class);
+                textHumidity = (TextView)view.findViewById(R.id.textA);
+                textHumidity.setText(hum + " %");
+            }
+            @Override
+            public void onCancelled(DatabaseError databaseError) {
+                Log.w("LogFragment", "loadLog:onCancelled", databaseError.toException());
+            }
+
+        });
+
+        databaseTemperature.addValueEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange(DataSnapshot dataSnapshot) {
+                temp = dataSnapshot.getValue(float.class);
+                textTemperature = (TextView)view.findViewById(R.id.textB);
+                textTemperature.setText(temp + " C");
+
+                if(temp >= 25 && temp <= 28){
+                    imageView_Good.setImageResource(R.drawable.good2);
+                }else{
+                    imageView_Good.setImageResource(R.drawable.bad2);
+                }
+            }
+
+            @Override
+            public void onCancelled(DatabaseError databaseError) {
+                Log.w("LogFragment", "loadLog:onCancelled", databaseError.toException());
+            }
+        });
     }
 }
-
-//    private FirebaseDatabase firebaseDatabase = FirebaseDatabase.getInstance();
-//    private DatabaseReference databaseHumidity = firebaseDatabase.getReference().child("Humidity");
-//    private DatabaseReference databaseTemperature = firebaseDatabase.getReference().child("Temperature");
-//
-//    TextView textHumidity;
-//    TextView textTemperature;
-//
-//    @Override
-//    protected void onCreate(Bundle savedInstanceState) {
-//        super.onCreate(savedInstanceState);
-//        setContentView(R.layout.activity_main);
-//
-//        textHumidity = (TextView)findViewById(R.id.text1);
-//        textTemperature = (TextView)findViewById(R.id.text2);
-//    }
-//
-//    @Override
-//    protected void onStart() {
-//        super.onStart();
-//
-//        databaseHumidity.addValueEventListener(new ValueEventListener() {
-//            @Override
-//            public void onDataChange(DataSnapshot dataSnapshot) {
-//                float hum = dataSnapshot.getValue(float.class);
-//                textHumidity.setText(""+hum);
-//            }
-//
-//            @Override
-//            public void onCancelled(DatabaseError databaseError) {
-//
-//            }
-//        });
-//
-//        databaseTemperature.addValueEventListener(new ValueEventListener() {
-//            @Override
-//            public void onDataChange(DataSnapshot dataSnapshot) {
-//                float temp = dataSnapshot.getValue(float.class);
-//                textTemperature.setText(""+temp);
-//            }
-//
-//            @Override
-//            public void onCancelled(DatabaseError databaseError) {
-//
-//            }
-//        });
-//    }
--- app/src/main/java/arduino/ahn/com/arduino/Fragment3.java
+++ app/src/main/java/arduino/ahn/com/arduino/Fragment3.java
@@ -2,9 +2,18 @@ package arduino.ahn.com.arduino;
 
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
 
 /**
  * Created by user on 2018-06-01.
@@ -16,11 +25,104 @@ public class Fragment3 extends Fragment{
         // Required empty public constructor
     }
 
+    View view;
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_fragment3, container, false);
+
+        view = inflater.inflate(R.layout.fragment_fragment3, container, false);
+
+        textCO = (TextView)view.findViewById(R.id.textCO);
+        textNO2 = (TextView)view.findViewById(R.id.textNO2);
+        textNH3 = (TextView)view.findViewById(R.id.textNH3);
+
+        imageView_Good = (ImageView)view.findViewById(R.id.imageViewGas);
+
+        return view;
+    }
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+
+    private FirebaseDatabase firebaseDatabase = FirebaseDatabase.getInstance();
+    private DatabaseReference databaseCO = firebaseDatabase.getReference().child("CO");
+    private DatabaseReference databaseNO2 = firebaseDatabase.getReference().child("NO2");
+    private DatabaseReference databaseNH3 = firebaseDatabase.getReference().child("NH3");
+
+    TextView textCO;
+    TextView textNO2;
+    TextView textNH3;
+
+    ImageView imageView_Good;
+
+    float CO;
+    float NO2;
+    float NH3;
+
+    @Override
+    public void onStart() {
+        super.onStart();
+
+        databaseCO.addValueEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange(DataSnapshot dataSnapshot) {
+
+                CO = dataSnapshot.getValue(float.class);
+                String CO_String = String.format("%.2f", CO);
+                float CO_float = Float.parseFloat(CO_String);
+
+                textCO = (TextView)view.findViewById(R.id.textCO);
+                textCO.setText(CO_float + " ppm");
+
+                if(CO <= 9){
+                    imageView_Good.setImageResource(R.drawable.good2);
+                }else{
+                    imageView_Good.setImageResource(R.drawable.bad2);
+                }
+
+            }
+            @Override
+            public void onCancelled(DatabaseError databaseError) {
+                Log.w("LogFragment", "loadLog:onCancelled", databaseError.toException());
+            }
+
+        });
+
+        databaseNO2.addValueEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange(DataSnapshot dataSnapshot) {
+                NO2 = dataSnapshot.getValue(float.class);
+                String NO2_String = String.format("%.2f", NO2);
+                float NO2_float = Float.parseFloat(NO2_String);
+
+                textNO2 = (TextView)view.findViewById(R.id.textNO2);
+                textNO2.setText(NO2_float + " ppm");
+            }
+
+            @Override
+            public void onCancelled(DatabaseError databaseError) {
+                Log.w("LogFragment", "loadLog:onCancelled", databaseError.toException());
+            }
+        });
+
+        databaseNH3.addValueEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange(DataSnapshot dataSnapshot) {
+                NH3 = dataSnapshot.getValue(float.class);
+                String NH3_String = String.format("%.2f", NH3);
+                float NH3_float = Float.parseFloat(NH3_String);
+
+                textNH3 = (TextView)view.findViewById(R.id.textNH3);
+                textNH3.setText(NH3_float + " ppm");
+            }
+
+            @Override
+            public void onCancelled(DatabaseError databaseError) {
+                Log.w("LogFragment", "loadLog:onCancelled", databaseError.toException());
+            }
+        });
     }
 
 }
